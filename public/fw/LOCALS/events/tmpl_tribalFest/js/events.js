a2fe9cf9c3b09afbcf9123812cccd3f0b6835e7d
Ioana <ioana@serenitymedia.ro>
2013-05-26 09:31:58 +0300

    26 may

--- public/fw/LOCALS/events/tmpl_tribalFest/js/events.js
+++ public/fw/LOCALS/events/tmpl_tribalFest/js/events.js
@@ -1,3 +1,20 @@
+
+ivyMods.set_iEdit.events = function(){
+
+    iEdit.add_bttsConf({
+        'membr':
+        {
+            moduleName: 'events',
+            addBt:{status: false},
+            saveBt:{status: false},
+            deleteBt:{atrValue: 'delete Member'}
+
+        }
+    });
+
+};
+
+
 ivyMods.events = {
 
     //document.getElementById('captcha').src = 'assets/securimage/securimage_show.php?sid=' + Math.random(); this.blur(); return false
@@ -6,18 +23,25 @@ ivyMods.events = {
         return false;
     },
 
+    // show hide workshops
+    show_ws: function(){
+        $(this).find('*[class^=ev-fullDescription]').slideDown();
+
+    },
+    hide_ws: function(){
+        $(this).find('*[class^=ev-fullDescription]').slideUp();
+
+    },
     binds : function(){
 
-        $('.evs_ws .ev').on('mouseover', function(){
-            $(this).find('*[class^=ev-fullDescription]').slideDown();
-        });
-
-        $('.evs_ws .ev').on('mouseout', function(){
-            $(this).find('*[class^=ev-fullDescription]').slideUp();
+        $('.evs_ws .ev').hoverIntent({
+            over: ivyMods.events.show_ws,
+            out: ivyMods.events.hide_ws
         });
 
     }
 
+    // signup workshops
     ,show_sgup : function(idENT_ev){
 
           $('#'+idENT_ev).find('input[name=go_signup_ev]').hide();
