0645dc0590ee9980309c31c6687412ee90e9257b
Victor Nițu <victor@debian.org.ro>
2013-07-18 21:17:34 +0300

    Added login switch in HTTP request

---
+++ public/fw/LOCALS/blogSite/tmpl_bsea/tmpl/footer.php
@@ -0,0 +1,22 @@
+
+<!-- begin footer -->
+<div class="fullwidth lightgrey">
+    <div class="block overflow footerText">
+        The Black Sea by
+        <a href="http://creativecommons.org/choose/theblacksea.eu" target="_blank">crji.org</a>
+        is licensed under a
+        <a href-"http://creativecommons.org/licenses/by-sa/3.0/deed.en_US" target="_blank">Creative Commons Attribution-ShareAlike 3.0 Unported License</a>
+        if not otherwise stated. Based on a work at
+        <a href="http://creativecommons.org/choose/theblacksea.eu" target="_blank">theblacksea.eu</a>.
+        This web application is Free Software (<a href="http://www.gnu.org/licenses/agpl-3.0.html" target="_blank">AGPLv3+</a>),
+        source code is <a href="http://git.ceata.org/cgit.cgi/open-media-challenge.git/" target="_blank">available</a>
+        and waiting for contributions.
+    </div>
+</div>
+<!-- end footer -->
+
+<!-- end mainContainer -->
+
+<?php
+    isset($_GET['login']) && include "login.html";
+?>
