8d37ea887371479c7671882812d950ca33bd12d4
Victor Nițu <victor@debian.org.ro>
2013-07-24 19:14:09 +0300

    Compatibility with latest development version of Ivy framework

--- public/index.php
+++ public/index.php
@@ -1,13 +1,14 @@
 <?php
-error_log("[ ivy ] "."/////////////////////////////////////////////////////////////////////////////////////////////////");
-error_log("[ ivy ] "."//////////////////////////////// [ BlackSea page load ] /////////////////////////////////////////");
-error_log("[ ivy ] "."/////////////////////////////////////////////////////////////////////////////////////////////////");
-error_log("[ ivy ] "."                                                                                                 ");
-error_log("[ ivy ] "."                                                                                                 ");
-#xdebug_start_trace('../trace.txt');
+error_log("[ ivy ] "."/////////////////////////////////////////////////");
+error_log("[ ivy ] "."//////////// [ BlackSea page load ] /////////////");
+error_log("[ ivy ] "."/////////////////////////////////////////////////");
+error_log("[ ivy ] "."                                                 ");
+error_log("[ ivy ] "."                                                 ");
 
-    require_once('../protected/etc/config.php');
-    require_once(FW_INC_PATH.'GENERAL/core/scripts/LOG.php');
+//xdebug_start_trace('../trace.txt');
+
+    require_once '../protected/etc/config.php';
+    require_once FW_INC_PATH.'GENERAL/core/scripts/ivyStart.php';
     $profiler = new PhpQuickProfiler(PhpQuickProfiler::getMicroTime());
 
 //error_reporting(E_ALL);
@@ -16,6 +17,7 @@ error_log("[ ivy ] "."                                                          
 //echo $_SESSION['auth']->name;
 //var_dump($_SESSION['auth']);
 
-#xdebug_stop_trace();
-require_once(FW_INC_PATH.'GENERAL/core/scripts/index.php');
+//xdebug_stop_trace();
+
+require_once FW_INC_PATH.'GENERAL/core/scripts/index.php';
 
