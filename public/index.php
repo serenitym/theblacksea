760d97051c7cc57e0f8776d1fd91e40b45143c01
Victor Nițu <victor@debian.org.ro>
2013-07-31 03:17:22 +0300

    Broken config into environments (1)

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ error_log("[ ivy ] "."                                                 ");
 
 //xdebug_start_trace('../trace.txt');
 
-    require_once '../protected/etc/config.php';
+    require_once '../protected/etc/config.base.php';
     require_once FW_INC_PATH.'GENERAL/core/scripts/ivyStart.php';
     $profiler = new PhpQuickProfiler(PhpQuickProfiler::getMicroTime());
 
