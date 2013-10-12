4ae7ae2dd5ff5e898212baa83aa7703c0f93ade8
Victor Nițu <victor@debian.org.ro>
2013-10-12 15:35:12 +0300

    Enforced the favicon (was it necessary?)

--- public/fw/LOCALS/blogSite/tmpl_bsea/tmpl/header.php
+++ public/fw/LOCALS/blogSite/tmpl_bsea/tmpl/header.php
@@ -2,3 +2,5 @@
 <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 
 <title><?php echo SITE_NAME; ?></title>
+
+<link rel="shortcut icon" href="http://serenitymedia.ro/uploads/favicon.png" type="image/x-icon" />
