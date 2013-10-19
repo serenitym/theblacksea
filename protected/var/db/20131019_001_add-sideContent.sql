0f3872b4d7a18c178664d510049e7b34360769fc
Ioana <ioana@serenitymedia.ro>
2013-10-19 03:48:16 +0300

    adding sideContent

---
+++ protected/var/db/20131019_001_add-sideContent.sql
@@ -0,0 +1,10 @@
+ALTER TABLE  `blogRecords` ADD  `sideContent` LONGTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL AFTER  `content` ;
+
+CREATE OR REPLACE VIEW `blogRecords_view` AS select `blogRecords`.`idRecord` AS `idRecord`,`blogRecords`.`idCat` AS `idCat`,`blogRecords`.`idTree` AS `idTree`,`blogRecords`.`uidRec` AS `uidRec`,`blogRecords`.`title` AS `title`
+,`blogRecords`.`content` AS `content`
+,`blogRecords`.`sideContent` AS `sideContent`
+,`blogRecords`.`lead` AS `lead`
+
+,`blogRecords`.`leadSec` AS `leadSec`
+,`blogRecords`.`country` AS `country`
+,`blogRecords`.`city` AS `city`,`blogRecords_stats`.`entryDate` AS `entryDate`,`blogRecords_stats`.`publishDate` AS `publishDate`,`blogRecords_stats`.`nrRates` AS `nrRates`,`blogRecords_stats`.`ratingTotal` AS `ratingTotal`,`blogRecords_stats`.`republish` AS `republish`,`blogRecords_settings`.`relatedStory` AS `relatedStory`,`blogRecords_settings`.`css` AS `css`,`blogRecords_settings`.`js` AS `js`,`blogRecords_settings`.`SEO` AS `SEO`,`blogRecord_folders`.`folderName` AS `folderName`,`blogRecord_folders`.`idFolder` AS `idFolder`,`blogRecord_formats`.`format` AS `format`,`blogRecord_formats`.`idFormat` AS `idFormat` from ((((`blogRecords` join `blogRecords_stats` on((`blogRecords`.`idRecord` = `blogRecords_stats`.`idRecord`))) left join `blogRecords_settings` on((`blogRecords`.`idRecord` = `blogRecords_settings`.`idRecord`))) left join `blogRecord_folders` on((`blogRecords_settings`.`idFolder` = `blogRecord_folders`.`idFolder`))) left join `blogRecord_formats` on((`blogRecords_settings`.`idFormat` = `blogRecord_formats`.`idFormat`)));
