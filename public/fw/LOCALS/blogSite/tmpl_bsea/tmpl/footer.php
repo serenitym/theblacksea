23921ce4edd14f1f846104dcb22c528808758335
Victor Nițu <victor@debian.org.ro>
2013-10-20 08:44:13 +0300

    Production: Added Piwik

--- public/fw/LOCALS/blogSite/tmpl_bsea/tmpl/footer.php
+++ public/fw/LOCALS/blogSite/tmpl_bsea/tmpl/footer.php
@@ -18,3 +18,23 @@
 
 <!-- end mainContainer -->
 
+<?php if(defined('ENV') && ENV == 'production'): ?>
+
+    <!-- Piwik -->
+    <script type="text/javascript">
+      var _paq = _paq || [];
+      _paq.push(['trackPageView']);
+      _paq.push(['enableLinkTracking']);
+      (function() {
+        var u=(("https:" == document.location.protocol) ? "https" : "http") + "://piwik.theblacksea.eu//";
+        _paq.push(['setTrackerUrl', u+'piwik.php']);
+        _paq.push(['setSiteId', 1]);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript';
+        g.defer=true; g.async=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+      })();
+
+    </script>
+    <noscript><p><img src="http://piwik.theblacksea.eu/piwik.php?idsite=1" style="border:0" alt="" /></p></noscript>
+    <!-- End Piwik Code -->
+
+<?php endif; ?>
